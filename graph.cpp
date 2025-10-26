e9cb961c53f46d93c69500f0f850b15c226fc58d
a7med-yamany <ahmed.m.yamany@aucegypt.edu>
2025-10-27 00:48:24 +0300

    Finished graph class.
    Finished graph class.

---
+++ graph.cpp
@@ -0,0 +1,152 @@
+//BY Ahmed Yamany
+#include "graph.h"
+#include <iostream> 
+#include <fstream>
+#include <sstream>
+
+Graph::Graph() : head(nullptr) {
+}
+
+Graph::~Graph() {
+    clear();
+}
+
+void Graph::clear() {
+    VertexNode* currVertex = head;
+
+    // Loop all Vertex nodes
+    while (currVertex) {
+        EdgeNode* currEdge = currVertex->adjHead;
+
+        // Delete all edges connected to this Vertex
+        while (currEdge) {
+            EdgeNode* nextEdge = currEdge->next; 
+            delete currEdge;  
+            currEdge = nextEdge;
+        }
+
+        VertexNode* nextVertex = currVertex->next;
+        delete currVertex;                         
+        currVertex = nextVertex; 
+    }
+
+    head = nullptr;
+}
+
+VertexNode* Graph::findVertexNode(const char* name) const {
+    VertexNode* temp = head;
+
+    // loop the Vertex list
+    while (temp) {
+        if (strcmp(temp->name, name) == 0)
+            return temp;   
+        temp = temp->next;
+    }
+
+    return nullptr;
+}
+
+void Graph::addVertex(const char* name) {
+    if (findVertexNode(name))
+        return;
+
+    VertexNode* newVertex = new VertexNode(name);
+
+    if (!head) {
+        head = newVertex;
+    } else {
+        VertexNode* temp = head;
+        while (temp->next)
+            temp = temp->next;
+
+        temp->next = newVertex;
+    }
+}
+
+void Graph::addEdge(const char* from, const char* to, int w) {
+    // check both cities exist or create if they don't
+    addVertex(from);
+    addVertex(to);
+
+    VertexNode* fromVertex = findVertexNode(from);
+    VertexNode* toVertex = findVertexNode(to);
+
+    EdgeNode* newEdge1 = new EdgeNode(to, w);
+
+    if (!fromVertex->adjHead)
+        fromVertex->adjHead = newEdge1;
+    else {
+        EdgeNode* temp = fromVertex->adjHead;
+        while (temp->next)
+            temp = temp->next;
+
+        temp->next = newEdge1;
+    }
+
+    // (Because this is an undirected graph)
+    EdgeNode* newEdge2 = new EdgeNode(from, w);
+
+    if (!toVertex->adjHead)
+        toVertex->adjHead = newEdge2;
+    else {
+        EdgeNode* temp = toVertex->adjHead;
+        while (temp->next)
+            temp = temp->next;
+        temp->next = newEdge2;
+    }
+}
+
+
+void Graph::display() const {
+    VertexNode* Vertex = head;
+
+    while (Vertex) {
+        std::cout << Vertex->name << " -> ";
+
+        EdgeNode* edge = Vertex->adjHead;
+        while (edge) {
+            std::cout << edge->to << " (" << edge->weight << ")";
+
+            if (edge->next)
+                std::cout << " -> ";
+
+            edge = edge->next;
+        }
+
+        std::cout << std::endl;
+        Vertex = Vertex->next;
+    }
+}
+
+void Graph::readDataset(const char* filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    bool firstLine = true;
+
+    while (std::getline(file, line)) {
+        if (line.empty()) continue;          // skip blank lines
+        if (firstLine) {                     // skip header line
+            firstLine = false;
+            continue;
+        }
+
+        std::stringstream ss(line);
+        std::string city1, city2;
+        int distance;
+
+        ss >> city1 >> city2 >> distance;
+
+        if (city1.empty() || city2.empty() || ss.fail())
+            continue;
+
+        addEdge(city1.c_str(), city2.c_str(), distance);
+    }
+
+    file.close();
+    std::cout << "Dataset successfully readed from: " << filename << std::endl;
+}
---
+++ graph.h
@@ -0,0 +1,52 @@
+//BY Ahmed Yamany
+#ifndef GRAPH_H 
+#define GRAPH_H
+
+#include <cstring>
+
+struct EdgeNode {
+    char to[64];   
+    int weight;    
+    EdgeNode* next;  
+
+    EdgeNode(const char* t, int w, EdgeNode* n = nullptr) {
+        strncpy(to, t, 63);
+        to[63] = '\0';     
+        weight = w;        
+        next = n;
+    }
+};
+
+//this is the list of the edgeNodes above
+struct VertexNode {
+    char name[64]; 
+    EdgeNode* adjHead;     // Pointer to the head of this vertex's adjacency list (its neighbors)
+    VertexNode* next; 
+
+    VertexNode(const char* n) {
+        strncpy(name, n, 63);
+        name[63] = '\0';   
+        adjHead = nullptr; 
+        next = nullptr;  
+    }
+};
+
+class Graph {
+private:
+    VertexNode* head;  
+
+    // function to find a vertex node by name
+    VertexNode* findVertexNode(const char* name) const;
+
+public:
+    Graph();  
+    ~Graph();  
+
+    void clear();          // Delete all vertices and edges
+    void addVertex(const char* name);        // Add a new vertex if it doesn’t exist
+    void addEdge(const char* from, const char* to, int w); // Add connection between two vertices
+    void display() const;
+    void readDataset(const char* filename);
+};
+
+#endif  
