ef543ceff485a5c4505cb1cca5ba374b10b97b3b
a7med-yamany <ahmed.m.yamany@aucegypt.edu>
2025-11-15 21:37:45 +0200

    Started shortestPath function and defined heapNode structure for pathfinding

--- src/searchShortestPath.cpp
+++ src/searchShortestPath.cpp
@@ -1,4 +1,15 @@
 #include "searchShortestPath.h"
 #include <iostream>
+#include <cstring>
 
+void shortestPath(Graph& graph, const char* start, const char* destination)
+{
+    int numNodes = graph.getNumOfNodes();
+
+    int* distances = new int [numNodes];
+    bool* visited = new bool [numNodes];
+    char (*previous) [64] = new char [numNodes][64];
+
+    
+}
 
--- src/searchShortestPath.h
+++ src/searchShortestPath.h
@@ -1,7 +1,24 @@
 #ifndef searchShortestPath_H
 #define searchShortestPath_H
-#include "graph.h"
+
 #include "MinHeap.h"
+#include "graph.h"
+struct heapNode
+{
+    char cityName[64];
+    int distance;
+
+    bool operator<(const heapNode& other) const {
+        return distance < other.distance;
+    }
+    bool operator>(const heapNode& other) const {
+        return distance > other.distance;
+    }
+};
+
+void shortestPath(Graph& graph, const char* start, const char* destination);
+
+
 
 
 
